83e2f7296eb904565cdb280d02706bf01a59d12f
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-11-13 17:33:47 -0600

    connect pages like login and register

--- book-finder/src/App.js
+++ book-finder/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Login from './components/LoginPage';
+import About from './components/About';
+import Contact from './components/Contact';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home </Link>
+          <Link to="/login">Login </Link>
+          <Link to="/about">About </Link>
+          <Link to="/contact">ContactUs</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+    // <div className="App">
+    //   <Home />
+    // </div>
   );
 }
 
+
 export default App;
+
+// <div className="App">
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header>
+// </div>
---
+++ book-finder/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import backgroundImage from './Books.jpg';
+
+const Home = () => {
+  const buttonStyle = {
+    position: 'absolute',
+    top: '50%',  
+    left: '50%',
+    transform: 'translate(200%, 80%)', 
+    backgroundColor: 'rgba(255, 255, 255, 0.7)', 
+    padding: '10px',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  };
+  return (
+    <div className='homepage'>
+      <img width={1500} height={690} src={backgroundImage} className="background-image" alt="backgroundImage" />
+      <a href="/login" style={buttonStyle}>Get started here!</a>
+    </div>
+  );
+};
+
+export default Home;
