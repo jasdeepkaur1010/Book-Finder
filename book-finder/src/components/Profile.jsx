8ac99c8537bcfbfc04c4d3b323642192e1e6dcc3
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-11-23 21:29:10 -0600

    add get azios route to check if sub id exist in api

--- book-finder/src/components/Profile.jsx
+++ book-finder/src/components/Profile.jsx
@@ -1,16 +1,35 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
   console.log(user, 'user');
 
+  const [subId, setSubId] = useState('');
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    console.log(user, 'usereffect')
+    const checkuser = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/users/${subId}`);
+        console.log('Sub_id exists', response.data);
+        setError('User with this sub_id already exists. Please choose a different one.');
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          console.log('Sub_id does not exist.');
+          setError(null); // Clear any previous errors
+        } else {
+          // Handle other error cases
+          console.error('Error checking sub_id existence:', error.message);
+        }
+      }
+    }
     if(user) {
     const postData = async () => {
       try {
+        await checkuser();
+        if (!error) {
         const postDataa = {
           username: user.nickname,
           email: user.email,
@@ -21,6 +40,7 @@ const Profile = () => {
         const response = await axios.post('http://localhost:8080/users', postDataa);
 
         console.log('Response', response.data);
+      }
       } catch (error) {
         console.error('Error posting data: ', error);
       }
