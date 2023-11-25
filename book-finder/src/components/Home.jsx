310e12af1698cc3a6fddc50c4323a24b5a933be7
RonnieGuo <guorongfly@gmail.com>
2023-11-25 15:07:32 -0600

    Add search bar and cards in Home page

--- book-finder/src/components/Home.jsx
+++ book-finder/src/components/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import backgroundImage from './Books.jpg';
+import React, { useState } from 'react';
+import BookCard from './BookCard';
+import "../styles/Home.scss";
 
 const Home = () => {
   const buttonStyle = {
@@ -12,10 +13,51 @@ const Home = () => {
     borderRadius: '5px',
     cursor: 'pointer',
   };
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  // Dummy book data for illustration
+  const books = [
+    { id: 1, title: 'Book 1', author: 'Author 1' },
+    { id: 2, title: 'Book 2', author: 'Author 2' },
+    // Add more books as needed
+  ];
+
+  // Function to handle search input
+  const handleSearch = (event) => {
+    const term = event.target.value.toLowerCase();
+    setSearchTerm(term);
+
+    // Filter books based on the search term
+    const results = books.filter(
+      (book) =>
+        book.title.toLowerCase().includes(term) ||
+        book.author.toLowerCase().includes(term)
+    );
+
+    setSearchResults(results);
+  };
+
   return (
     <div className='homepage'>
-      <img width={1500} height={690} src={backgroundImage} className="background-image" alt="backgroundImage" />
-      <a href="/login" style={buttonStyle}>Get started here!</a>
+      <h1>Book Finder</h1>
+        <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search for books..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+
+      <div className="book-cards">
+        {searchResults.map((book) => (
+          <BookCard key={book.id} title={book.title} author={book.author} />
+        ))}
+      </div>
+
+
+
     </div>
   );
 };
