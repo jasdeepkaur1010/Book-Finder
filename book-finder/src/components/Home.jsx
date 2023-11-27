c0fedb32f39735bc39a1af2c066d3d476ba11edb
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-11-26 20:48:36 -0600

    fixed package and package-lock and homejsx

--- book-finder/src/components/Home.jsx
+++ book-finder/src/components/Home.jsx
@@ -1,35 +1,51 @@
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import BookCard from './BookCard';
 import "../styles/Home.scss";
 import backgroundImage from './Books.jpg';
 import Profile from './Profile';
+import axios from 'axios';
 
 const Home = () => {
-  
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  
+  const handleSearch = async () => {
+    try {
+      // Make a request to your search-books API endpoint using axios
+      const response = await axios.get(`http://localhost:8080/api/search-books?query=${searchTerm}`);
+      
+      // Check if the request was successful
+      if (response.status === 200) {
+        // Update the searchResults state with the fetched results
+        setSearchResults(response.data);
+      } else {
+        throw new Error('Failed to fetch search results');
+      }
+    } catch (error) {
+      console.error('Error during search:', error.message);
+      // Handle the error, e.g., display an error message to the user
+    }
+  };
+  
+  useEffect(() => {
+    // You might want to add additional logic here if needed when the searchTerm changes
+    // For example, debouncing the search to avoid making requests on every keystroke
+  }, [searchTerm]);
+
 
   // Dummy book data for illustration
-  const books = [
-    { id: 1, title: 'Book 1', author: 'Author 1' },
-    { id: 2, title: 'Book 2', author: 'Author 2' },
-    // Add more books as needed
+  const dummyBooks = [
+    { id: 1, title: 'Book 1', author: 'Author 1', imageUrl: 'https://m.media-amazon.com/images/I/81Fyh2mrw4L._SY466_.jpg' },
+    { id: 1, title: 'Book 1', author: 'Author 1', imageUrl: 'https://m.media-amazon.com/images/I/81Fyh2mrw4L._SY466_.jpg' },
+    { id: 1, title: 'Book 1', author: 'Author 1', imageUrl: 'https://m.media-amazon.com/images/I/81Fyh2mrw4L._SY466_.jpg' },
+    { id: 1, title: 'Book 1', author: 'Author 1', imageUrl: 'https://m.media-amazon.com/images/I/81Fyh2mrw4L._SY466_.jpg' },
+    { id: 1, title: 'Book 1', author: 'Author 1', imageUrl: 'https://m.media-amazon.com/images/I/81Fyh2mrw4L._SY466_.jpg' },
+    { id: 1, title: 'Book 2', author: 'Author 2', imageUrl: 'https://m.media-amazon.com/images/I/41SKsBaGXRL._SY445_SX342_.jpg' },
+    { id: 1, title: 'Book 2', author: 'Author 2', imageUrl: 'https://m.media-amazon.com/images/I/41SKsBaGXRL._SY445_SX342_.jpg' },
+    { id: 1, title: 'Book 2', author: 'Author 2', imageUrl: 'https://m.media-amazon.com/images/I/41SKsBaGXRL._SY445_SX342_.jpg' },
+
   ];
-
-  // Function to handle search input
-  const handleSearch = (event) => {
-    const term = event.target.value.toLowerCase();
-    setSearchTerm(term);
-
-    // Filter books based on the search term
-    const results = books.filter(
-      (book) =>
-        book.title.toLowerCase().includes(term) ||
-        book.author.toLowerCase().includes(term)
-    );
-
-    setSearchResults(results);
-  };
 
   return (
     <div className='homepage'>
@@ -50,8 +66,23 @@ const Home = () => {
       </div>
 
       <Profile />
-      <img width={1500} height={690} src={backgroundImage} className="background-image" alt="backgroundImage" />
-      <a href="/login" style={buttonStyle}>Get started here!</a>
+      <h1>Book Finder</h1>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search for books..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      <div className="book-cards">
+        {dummyBooks.map((book) => (
+          <BookCard key={book.id} title={book.title} author={book.author} imageUrl={book.imageUrl} />
+        ))}
+      </div>
+      {/* <img width={1500} height={690} src={backgroundImage} className="background-image" alt="backgroundImage" />
+      <a href="/login" style={buttonStyle}>Get started here!</a> */}
     </div>
   );
 };
