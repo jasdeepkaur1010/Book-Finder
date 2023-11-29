7efd57a39db291418f522963ba2ce70d48ba7346
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-11-28 23:17:58 -0600

    add modal for bookslist

--- book-finder/src/components/LibraryList.jsx
+++ book-finder/src/components/LibraryList.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+// import  './../styles/LibraryList.scss';
 function LibraryList() {
   const [libraries, setLibraries] = useState([]);
   const [books, setBooks] = useState([]);
+  const [selectedLibraryId, setSelectedLibraryId] = useState(null);
+  const [selectedLibrary, setSelectedLibrary] = useState(null);
   useEffect(() => {
     axios.get('http://localhost:8080/libraries')
     .then(response => {
@@ -14,10 +16,10 @@ function LibraryList() {
       console.error('Error fetching libraries:', error);
     });
   }, []);
-   const fetchBooksByLibrary = async (libraryId) => {
+   const fetchBooksByLibraryId = async (libraryId) => {
     try {
-      const response = await axios.get(`http://localhost:8080/libraries/${libraryId}`)
-      setBooks(response.data.libraries.books);
+      const response = await axios.get(`http://localhost:8080/libraries/${libraryId}/books`)
+      setBooks(response.data.books);
     }
     catch(error) {
       console.error('Error fetching books for a library:', error);
@@ -25,8 +27,16 @@ function LibraryList() {
     }
   };
   const handleLibraryClick = (libraryId) => {
-    fetchBooksByLibrary(libraryId);
+    setSelectedLibraryId(libraryId);
+    fetchBooksByLibraryId(libraryId);
+    setSelectedLibrary(libraryId);
   };
+
+  const closeModal = () => {
+    setSelectedLibrary(null);
+    setBooks([]);
+  };
+
 
   return (
     <div>
@@ -35,9 +45,9 @@ function LibraryList() {
         {libraries.map(library => (
           <li key={library.id}>
             <div>
-            <a href="#" onClick={() => handleLibraryClick(library.id)}>
+            <button onClick={() => handleLibraryClick(library.id)}>
               <img src={library.cover_photo} alt={library.name} />
-            </a>
+            </button>
             <div>
               <h3>Name: {library.name}</h3>
               <p>Status: {library.status}</p>
@@ -50,8 +60,31 @@ function LibraryList() {
           </li>
         ))}
       </ul>
+
+      {selectedLibraryId && selectedLibrary && books && books.length > 0 && (
+             <div className="modal">
+             <div className="modal-content">
+               <span className="close" onClick={closeModal}>&times;</span>
+          <h2>Book List</h2>
+          <ul>
+            {books.map(book => (
+              <li key={book.id}>
+                <div>
+                  <h3>{book.title}</h3>
+                  <p>author_id: {book.author_id}</p>
+                  <p>Genre: {book.genre}</p>
+                  <p>Summary: {book.summary}</p>
+                  <p>Publication_date: {book.publication_date}</p>
+                  <p></p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        </div>
+      )}
     </div>
   );
-   }
+}
 
 export default LibraryList;
