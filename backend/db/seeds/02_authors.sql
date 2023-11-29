78348911309b69dfde69e2af006b47bb14419e2a
RonnieGuo <guorongfly@gmail.com>
2023-11-28 22:55:29 -0600

    add seeds

--- backend/db/seeds/02_authors.sql
+++ backend/db/seeds/02_authors.sql
@@ -1,5 +1,8 @@
 INSERT INTO authors (full_name, status) VALUES
   ('John Smith', 'Active'),
   ('Alice Johnson', 'Inactive'),
+  ('Kavita Ganesan', 'Active'),
+  ('Malay A Upadhyay', 'Active'),
+  ('Will Scott', 'Active'),
   ('Emily Davis', 'Active');
 
