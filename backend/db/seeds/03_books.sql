d0f3f97e01a87694ac24df7118a42da924f6ccc1
RonnieGuo <guorongfly@gmail.com>
2023-11-28 19:18:25 -0600

    added seeds

--- backend/db/seeds/03_books.sql
+++ backend/db/seeds/03_books.sql
@@ -1,5 +1,7 @@
 INSERT INTO books (title, author_id, publication_date, genre, isbn, cover_image_url, summary, rating) VALUES
-  ('Title 1', 1, '2022-10-15', 'Fiction', '9780123456789', 'https://example.com/cover1.jpg', 'Summary of Book 1', 4.2),
-  ('Title 2', 2, '2021-07-20', 'Mystery', '9781234567890', 'https://example.com/cover2.jpg', 'Summary of Book 2', 3.8),
-  ('Title 3', 3, '2023-03-05', 'Science Fiction', '9782345678901', 'https://example.com/cover3.jpg', 'Summary of Book 3', 4.5),
+  ('Java : a beginners guide', 1, '2017-10-15', 'Java (Computer program language)', '9781260463552', 'https://ds.sasklibraries.ca/cover/l/3149982', 'Summary of Book 1', 4.2),
+  ('Learning Python Mark Lutz', 2, '2021-07-20', 'Python (Computer program language)', '0596513984', 'https://ds.sasklibraries.ca/cover/l/3144069', 'Describes the features of the Python 2.5 programming language, covering such topics as types and operations, statements and syntax, functions, modules, classes and OOP, and exceptions and tools.', 3.8),
+  ('50 years of silence / Jan Ruff-OHerne', 3, '2023-03-05', 'Science Fiction', '1875892001', 'https://ds.sasklibraries.ca/cover/l/425874', 'Summary of Book 3', 4.5),
+  ('Core Java. Volume 2, Advanced features / Cay S. Horstmann.', 3, '2023-03-05', 'Java (Computer program language)', '9780135166314', 'https://example.com/cover3.jpg', '"Core Java" is the leading no-nonsense tutorial and reference for experienced programmers who want to write robust Java code for real-world applications. Now, "Core Java, volume II, advanced features, eleventh edition" has been revised to reflect Java II, with complete coverage of advanced UI and enterprise programming, networking, security, and Java\''s powerful new module system.', 4.5), 
+  ('Title 3', 3, '2023-03-05', 'Science Fiction', '9782345678901', 'https://example.com/cover3.jpg', 'Summary of Book 3', 4.5), 
    ('Learn Java in One Day and Learn It Well', 3, '2023-03-05', 'Science', '9782345678', 'https://m.media-amazon.com/images/I/61KFmcmEK1L._SY466_.jpg', 'Complex concepts are broken down into simple steps to ensure that you can easily master the Java language even if you have never coded before.', 4.5);
